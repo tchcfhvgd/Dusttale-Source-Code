6e8e88d7c8b52de1537c12e40a5644e95d696131
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-27 00:05:37 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -80,8 +80,15 @@ class Main extends Sprite
 		
 		addChild(game);
 
+		FlxG.signals.preStateSwitch.add(function () {
+			if (!Main.skipNextDump) {
+				Paths114514.clearStoredMemory(true);
+				FlxG.bitmap.dumpCache();
+			}
+		});
 		FlxG.signals.postStateSwitch.add(function () {
 			Paths114514.clearUnusedMemory();
+			Main.skipNextDump = false;
 		});
 		
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
